154edf8e22323d3eb7e1faf63c4544869a0441dd
Amit Kumar Ghosh <Amit_kumar_Ghosh@hotmail.com>
2020-04-18 11:40:32 -0700

    SortedSquareArray with negatice integers

---
+++ Algorithms/SortSquareArray/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SortSquareArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { -6, -4, 1, 2, 3, 5 };
+            //int[] arr = new int[] { 1, 2, 4 };
+            var result = SortedArray(arr);
+            foreach (var number in result)
+            {
+                Console.Write(number + " ");
+
+            }
+            Console.WriteLine();
+        }
+
+        public static int[] SortedArray(int[] inputarray)
+        {
+            int[] temp = new int[inputarray.Length];
+            int left = 0;
+            int right = inputarray.Length - 1;
+            for (int i = 0; i < inputarray.Length; i++)
+            {
+                if (Math.Abs(inputarray[left]) > Math.Abs(inputarray[right]))
+                {
+                    temp[inputarray.Length - i - 1] = (int)Math.Pow(inputarray[left], 2);
+                    left++;
+                }
+                else
+                {
+                    temp[inputarray.Length - i - 1] = (int)Math.Pow(inputarray[right], 2);
+                    right--;
+                }
+            }
+            //Array.Sort(temp);
+            return temp;
+        }
+    }
+}
